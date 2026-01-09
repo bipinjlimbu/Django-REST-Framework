0f539700605e1142b6246c5f749ad9b49adf839b
bipinjlimbu <bipinlimbu712@gmail.com>
2026-01-09 11:06:10 +0545

    Database Connected

--- rest_project/settings.py
+++ rest_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'rest_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rest_db',
+        'HOST' : 'localhost',
+        'USER' : 'root',
+        'PASSWORD' : '',
+        'PORT' : 3306,
     }
 }
 
