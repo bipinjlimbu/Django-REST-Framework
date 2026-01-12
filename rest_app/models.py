c1e8850e42ab4ed4098a989e6192499c3a6c6e4b
bipinjlimbu <bipinlimbu712@gmail.com>
2026-01-12 11:10:07 +0545

    Product Model Created

--- rest_app/models.py
+++ rest_app/models.py
@@ -3,3 +3,9 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     phone = models.CharField(max_length=15,blank=True,null=True)
+    
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    category = models.CharField(max_length=100,blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
