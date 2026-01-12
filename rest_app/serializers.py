915d44238ae11da42f1b5a671b824d9c1169fd30
bipinjlimbu <bipinlimbu712@gmail.com>
2026-01-12 11:57:32 +0545

    Serializers and Urls Updated

--- rest_app/serializers.py
+++ rest_app/serializers.py
@@ -4,4 +4,4 @@ from .models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['title','category','price']
+        fields = '__all__'
--- rest_app/urls.py
+++ rest_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views.main_view import product_view
+from .views.main_view import product_view, product_detail_view
 
 urlpatterns = [
     path('products/', product_view),
+    path('products/<int:id>/', product_detail_view),
 ]
