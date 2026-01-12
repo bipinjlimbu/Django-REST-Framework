e0a2aa5b170c441ab5c518f4af2154715c2bcb1f
bipinjlimbu <bipinlimbu712@gmail.com>
2026-01-12 11:59:44 +0545

    PUT Method Done in product detail GET PUT DELETE

--- rest_app/views/main_view.py
+++ rest_app/views/main_view.py
@@ -32,3 +32,10 @@ def product_detail_view(request,id):
         serializer = ProductSerializer(product)
         return Response(serializer.data,status=status.HTTP_200_OK)
     
+    if request.method == 'PUT':
+        serializer = ProductSerializer(product,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':'Data updated successfully','data':serializer.data},status=status.HTTP_200_OK)
+        else:
+            return Response({'error':serializer.errors},status=status.HTTP_400_BAD_REQUEST)
